1706cea67df6beea546c6394d5225fd5eaab59c9
hngfu <djsl1018@naver.com>
2019-10-21 19:18:56 +0900

    Add padding to RoundCornerTextField

--- Todo-S/View/CustomView/RoundCornerTextField.swift
+++ Todo-S/View/CustomView/RoundCornerTextField.swift
@@ -24,5 +24,8 @@ class RoundCornerTextField: UITextField {
         self.layer.cornerRadius = self.layer.bounds.height / 2
         self.layer.borderWidth = 1
         self.layer.borderColor = UIColor.lightGray.cgColor
+        let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: 4, height: self.frame.height))
+        self.leftView = paddingView
+        self.leftViewMode = .always
     }
 }
