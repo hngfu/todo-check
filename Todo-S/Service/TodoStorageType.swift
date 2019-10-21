fbda4d423fac5d1d2e7f54f72116d036f004cdbc
hngfu <djsl1018@naver.com>
2019-10-21 22:22:19 +0900

    Add TodoStorageType

---
+++ Todo-S/Service/TodoStorageType.swift
@@ -0,0 +1,14 @@
+//
+//  TodoStorageType.swift
+//  Todo-S
+//
+//  Created by 조재흥 on 2019/10/21.
+//  Copyright © 2019 Hngfu. All rights reserved.
+//
+
+import Foundation
+
+protocol TodoStorageType {
+    
+    
+}
