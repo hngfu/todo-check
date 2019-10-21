31ad0c6defae9e9e078b8051dc37a4aa1e3b370e
hngfu <djsl1018@naver.com>
2019-10-21 13:53:53 +0900

    Add Project, Pod dependency, .gitignore

---
+++ Podfile
@@ -0,0 +1,12 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'Todo-S' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for Todo-S
+  pod 'RxSwift'
+  pod 'RxCocoa'
+  pod 'Action'
+end
---
+++ Todo-S/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Todo-S
+//
+//  Created by 조재흥 on 2019/10/21.
+//  Copyright © 2019 Hngfu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
