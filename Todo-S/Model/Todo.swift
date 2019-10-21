fce02bd6951e7b9ce524c0e2679f632c99767ca9
hngfu <djsl1018@naver.com>
2019-10-21 22:21:37 +0900

    Add Todo model

---
+++ Todo-S/Model/Todo.swift
@@ -0,0 +1,16 @@
+//
+//  Todo.swift
+//  Todo-S
+//
+//  Created by 조재흥 on 2019/10/21.
+//  Copyright © 2019 Hngfu. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+import RxDataSources
+
+class Todo: Object, IdentifiableType {
+    @objc dynamic var content: String = ""
+    @objc dynamic var identity: String = "\(Date().timeIntervalSinceReferenceDate)"
+}
