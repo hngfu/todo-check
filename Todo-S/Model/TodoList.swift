9006c75bc8f7801861a2b1f45a1c565beb2214f3
hngfu <djsl1018@naver.com>
2019-10-22 17:05:38 +0900

    Add TodoList model

---
+++ Todo-S/Model/TodoList.swift
@@ -0,0 +1,14 @@
+//
+//  TodoList.swift
+//  Todo-S
+//
+//  Created by 조재흥 on 2019/10/22.
+//  Copyright © 2019 Hngfu. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+class TodoList: Object {
+    let todos = List<Todo>()
+}
