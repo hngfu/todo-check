77c482cfdd480ca1e34d087794e386da3c1c9374
hngfu <djsl1018@naver.com>
2019-10-23 15:32:53 +0900

    Add TodoListViewModel

---
+++ Todo-S/ViewModel/TodoListViewModel.swift
@@ -0,0 +1,34 @@
+//
+//  TodoListViewModel.swift
+//  Todo-S
+//
+//  Created by 조재흥 on 2019/10/22.
+//  Copyright © 2019 Hngfu. All rights reserved.
+//
+
+import Foundation
+import RxDataSources
+import RxSwift
+import RxCocoa
+import Action
+
+class TodoListViewModel {
+    private let storage: TodoStorageType! = RealmStorage()
+    
+    let dataSource = RxTableViewSectionedAnimatedDataSource<TodoSectionModel> (configureCell: { dataSource, tableView, indexPath, todo in
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = todo.content
+        return cell
+    })
+    
+    var todoList: Observable<[TodoSectionModel]> {
+        return storage.todoList()
+    }
+    
+    lazy var createAction: Action<String, Void> = {
+        return Action<String, Void> { content in
+            self.storage.createTodo(content: content, completed: false)
+            return Observable.empty()
+        }
+    }()
+}
