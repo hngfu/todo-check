6697260aa9a074ac09b9cee243eb3765bdcf52a8
hngfu <djsl1018@naver.com>
2019-10-26 14:51:04 +0900

    Add ToDoTableViewCell

---
+++ Todo-S/View/CustomView/ToDoTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ToDoTableViewCell.swift
+//  Todo-S
+//
+//  Created by 조재흥 on 2019/10/24.
+//  Copyright © 2019 Hngfu. All rights reserved.
+//
+
+import UIKit
+
+class ToDoTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
