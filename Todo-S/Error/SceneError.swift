b37937e3375bada731266ecd1f1662da71798fa7
hngfu <djsl1018@naver.com>
2019-10-28 17:39:56 +0900

    Add SceneError

---
+++ Todo-S/Error/SceneError.swift
@@ -0,0 +1,13 @@
+//
+//  SceneError.swift
+//  Todo-S
+//
+//  Created by 조재흥 on 2019/10/28.
+//  Copyright © 2019 Hngfu. All rights reserved.
+//
+
+import Foundation
+
+enum SceneError: Error {
+    case failedToInstantiate
+}
