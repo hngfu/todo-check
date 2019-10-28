4c44ffbd4b62f68b783cfd0eca9803f3e425d60e
hngfu <djsl1018@naver.com>
2019-10-28 17:38:58 +0900

    Add CompletedToDoListViewModel

---
+++ Todo-S/ViewModel/CompletedToDoListViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  CompletedToDoListViewModel.swift
+//  Todo-S
+//
+//  Created by 조재흥 on 2019/10/28.
+//  Copyright © 2019 Hngfu. All rights reserved.
+//
+
+import Foundation
+
+class CompletedToDoListViewModel: ToDoViewModel {
+    
+}
