6380609d8001981657a617d9140fd1ea3716d99c
hngfu <djsl1018@naver.com>
2019-10-28 17:41:50 +0900

    Update application(_: didFinishLaunchingWithOptions: ) -> Bool in AppDelegate
    coordinator를 이용하여 scene을 이동하도록 함

--- Todo-S/AppDelegate.swift
+++ Todo-S/AppDelegate.swift
@@ -14,7 +14,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        let storage = RealmStorage()
+        let coordinator = SceneCoordinator(window: window!)
+        let viewModel = ToDoListViewModel(title: "To Do",
+                                          coordinator: coordinator,
+                                          storage: storage!)
+        let scene = Scene.toDoList(viewModel)
+        coordinator.transition(to: scene, using: .root, animated: false)
         return true
     }
 }
