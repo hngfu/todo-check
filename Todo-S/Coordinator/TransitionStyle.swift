742d9f9a95e741e522e7f7494f2c1a55402dec38
hngfu <djsl1018@naver.com>
2019-10-28 17:39:19 +0900

    Add TransitionStyle

---
+++ Todo-S/Coordinator/TransitionStyle.swift
@@ -0,0 +1,15 @@
+//
+//  TransitionStyle.swift
+//  Todo-S
+//
+//  Created by 조재흥 on 2019/10/28.
+//  Copyright © 2019 Hngfu. All rights reserved.
+//
+
+import Foundation
+
+enum TransitionStyle {
+    case root
+    case push
+    case modal
+}
