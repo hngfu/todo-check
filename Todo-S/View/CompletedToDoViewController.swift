6758553ea24540dc79a08d567b1e9f8e2d045de8
hngfu <djsl1018@naver.com>
2019-11-02 18:05:13 +0900

    Implement CompletedToDoViewController

--- Todo-S/View/CompletedToDoViewController.swift
+++ Todo-S/View/CompletedToDoViewController.swift
@@ -7,20 +7,41 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
+import RxDataSources
 
 class CompletedToDoViewController: UIViewController, ViewModelBindableType {
 
     static let identifier = "CompletedToDoListViewController"
     
-    var viewModel: CompletedToDoListViewModel!
+    @IBOutlet weak var completedToDoListTableView: UITableView!
+    @IBOutlet weak var deleteButton: UIBarButtonItem!
+    @IBOutlet weak var shareButton: UIBarButtonItem!
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
+    var viewModel: CompletedToDoListViewModel!
+    private let disposeBag = DisposeBag()
     
     func bindViewModel() {
+        viewModel.title
+            .drive(navigationItem.rx.title)
+            .disposed(by: disposeBag)
         
+        viewModel.toDoList
+            .bind(to: completedToDoListTableView.rx.items(dataSource: viewModel.dataSource))
+            .disposed(by: disposeBag)
+        
+        deleteButton.rx.tap.asDriver()
+            .drive(onNext: {
+                let alert = UIAlertController(title: nil, message: nil,
+                                              preferredStyle: .actionSheet)
+                let cancelAction = UIAlertAction(title: "취소", style: .cancel)
+                var deleteAction = UIAlertAction(title: "완료된 To Do 모두 삭제", style: .destructive)
+                deleteAction.rx.action = self.viewModel.makeDeleteAction()
+                alert.addAction(cancelAction)
+                alert.addAction(deleteAction)
+                self.present(alert, animated: true, completion: nil)
+            })
+            .disposed(by: disposeBag)
     }
 }
