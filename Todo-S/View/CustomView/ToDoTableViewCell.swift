1b3edc34c5d40171de2c574b35eaf4485f5d8153
hngfu <djsl1018@naver.com>
2019-11-10 20:43:28 +0900

    Modify ToDoTableViewCell UI

--- Todo-S/View/CustomView/ToDoTableViewCell.swift
+++ Todo-S/View/CustomView/ToDoTableViewCell.swift
@@ -12,15 +12,8 @@ class ToDoTableViewCell: UITableViewCell {
     static let identifier = "toDoTableViewCell"
     static let nibName = "ToDoTableViewCell"
     
-    @IBOutlet weak var checkButton: UIButton!
     @IBOutlet weak var contentLabel: UILabel!
-    @IBOutlet weak var containerView: UIView!
-    
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        checkButton.layer.cornerRadius = 15
-        containerView.layer.cornerRadius = 12
-    }
+    @IBOutlet weak var checkButton: RoundCornerButton!
     
     func set(with toDo: ToDo) {
         self.contentLabel.text = toDo.content
