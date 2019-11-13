8ec734431043cfa990e16c713944e3307b91966e
hngfu <djsl1018@naver.com>
2019-11-13 18:04:37 +0900

    Add License Model

---
+++ Todo-S/Model/License.swift
@@ -0,0 +1,14 @@
+//
+//  License.swift
+//  ToDo S
+//
+//  Created by 조재흥 on 2019/11/13.
+//  Copyright © 2019 Hngfu. All rights reserved.
+//
+
+import Foundation
+
+struct License {
+    let title: String
+    let content: String
+}
