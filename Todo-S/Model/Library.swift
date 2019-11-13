69d7647d766c694aedd7aec027a4227069137002
hngfu <djsl1018@naver.com>
2019-11-13 18:04:28 +0900

    Add Library Model

---
+++ Todo-S/Model/Library.swift
@@ -0,0 +1,16 @@
+//
+//  Library.swift
+//  ToDo S
+//
+//  Created by 조재흥 on 2019/11/13.
+//  Copyright © 2019 Hngfu. All rights reserved.
+//
+
+import Foundation
+
+struct Library {
+    let title: String
+    let url: String
+    let copyright: String
+    let license: String
+}
